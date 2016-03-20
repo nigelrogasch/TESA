09b4c5a2127acebb32fe9d6b425aba678a895232
Nigel Rogasch <nigel.rogasch@gmail.com>
2016-03-21 08:48:05 +1100

    plugin function

--- eegplugin_tesa.m
+++ eegplugin_tesa.m
@@ -9,7 +9,7 @@
 %   catch_string  - [struct] "catch" strings for menu callbacks.
 %
 
-% Copyright (C) 2015 Nigel Rogasch, Monash University,
+% Copyright (C) 2016 Nigel Rogasch, Monash University,
 % nigel.rogasch@monash.edu
 %
 % This program is free software; you can redistribute it and/or modify
@@ -28,7 +28,7 @@
 
 function vers = eegplugin_tesa(fig, try_strings, catch_strings)
 
-    vers = 'tesa1.1';
+    vers = 'tesa1.0';
     if nargin < 3
         error('eegplugin_tesa requires 3 arguments');
     end
@@ -39,15 +39,42 @@ function vers = eegplugin_tesa(fig, try_strings, catch_strings)
 
     % menu callbacks
     % --------------
+    comfindpulse = [try_strings.no_check '[EEG LASTCOM] = pop_tesa_findpulse(EEG);' catch_strings.add_to_hist];
+    comfixtrigger = [try_strings.no_check '[EEG LASTCOM] = pop_tesa_fixtrigger(EEG);' catch_strings.add_to_hist];
     comremovedata = [try_strings.no_check '[EEG LASTCOM] = pop_tesa_removedata(EEG);' catch_strings.new_and_hist];
-    comsortcomps = [try_strings.no_check '[EEG LASTCOM] = pop_tesa_sortcomps(EEG);' catch_strings.new_and_hist];
-    compcacompress = [try_strings.no_check '[EEG LASTCOM] = pop_tesa_pcacompress(EEG);' catch_strings.new_and_hist];
     cominterpdata = [try_strings.no_check '[EEG LASTCOM] = pop_tesa_interpdata(EEG);' catch_strings.new_and_hist];
+    comfastica = [try_strings.no_check '[EEG LASTCOM] = pop_tesa_fastica(EEG);' catch_strings.new_and_hist];
+    comcompselect = [try_strings.no_check '[EEG LASTCOM] = pop_tesa_compselect(EEG);' catch_strings.new_and_hist];
+    comedm = [try_strings.no_check '[EEG LASTCOM] = pop_tesa_edm(EEG);' catch_strings.new_and_hist];
+    compcacompress = [try_strings.no_check '[EEG LASTCOM] = pop_tesa_pcacompress(EEG);' catch_strings.new_and_hist];
+    compcasuppress = [try_strings.no_check '[EEG LASTCOM] = pop_tesa_pcasuppress(EEG);' catch_strings.new_and_hist];
+    comdetrend = [try_strings.no_check '[EEG LASTCOM] = pop_tesa_detrend(EEG);' catch_strings.new_and_hist];
+    comfiltbutter = [try_strings.no_check '[EEG LASTCOM] = pop_tesa_filtbutter(EEG);' catch_strings.new_and_hist];
+    comtepextract = [try_strings.no_check '[EEG LASTCOM] = pop_tesa_tepextract(EEG);' catch_strings.add_to_hist];
+    compeakanalysis = [try_strings.no_check '[EEG LASTCOM] = pop_tesa_peakanalysis(EEG);' catch_strings.add_to_hist];
+    compeakoutput = [try_strings.no_check '[output LASTCOM] = pop_tesa_peakoutput(EEG);' catch_strings.add_to_hist];
+    complot = [try_strings.no_check '[LASTCOM] = pop_tesa_plot(EEG);' catch_strings.add_to_hist];
+    compeakoutputgroup = [try_strings.no_check '[output LASTCOM] = pop_tesa_peakoutputgroup(EEG);' catch_strings.add_to_hist];
+    complotgroup = [try_strings.no_check '[LASTCOM] = pop_tesa_plotgroup(EEG);' catch_strings.add_to_hist];
 
     % create menus
     % -------------------------
     submenu = uimenu( menu, 'Label', 'TMS-EEG signal analyser (TESA)', 'separator', 'on');
-    uimenu( submenu, 'Label', 'Remove artifact data'  , 'CallBack', comremovedata);
-    uimenu( submenu, 'Label', 'Sort components'  , 'CallBack', comsortcomps, 'separator', 'on');
-    uimenu( submenu, 'Label', 'PCA compress'  , 'CallBack', compcacompress);
-    uimenu( submenu, 'Label', 'Interpolate missing data'  , 'CallBack', cominterpdata, 'separator', 'on');
+    uimenu( submenu, 'Label', 'Find TMS pulse'  , 'CallBack', comfindpulse);
+    uimenu( submenu, 'Label', 'Fix TMS pulse latency'  , 'CallBack', comfixtrigger);
+    uimenu( submenu, 'Label', 'Remove artifact data'  , 'CallBack', comremovedata, 'separator', 'on');
+    uimenu( submenu, 'Label', 'Interpolate removed data'  , 'CallBack', cominterpdata);
+    uimenu( submenu, 'Label', 'FastICA'  , 'CallBack', comfastica, 'separator', 'on' );
+    uimenu( submenu, 'Label', 'Auto component selection'  , 'CallBack', comcompselect);
+    uimenu( submenu, 'Label', 'Enhanced deflation method (EDM)'  , 'CallBack', comedm);
+    uimenu( submenu, 'Label', 'PCA compression'  , 'CallBack', compcacompress);
+    uimenu( submenu, 'Label', 'PCA suppression'  , 'CallBack', compcasuppress);
+    uimenu( submenu, 'Label', 'Detrend data'  , 'CallBack', comdetrend);
+    uimenu( submenu, 'Label', 'Butterworth filter'  , 'CallBack', comfiltbutter, 'separator', 'on');
+    uimenu( submenu, 'Label', 'Extract TEPs'  , 'CallBack', comtepextract, 'separator', 'on');
+    uimenu( submenu, 'Label', 'Find and analyse TEP peaks'  , 'CallBack', compeakanalysis);
+    uimenu( submenu, 'Label', 'Output TEP peak analysis'  , 'CallBack', compeakoutput);
+    uimenu( submenu, 'Label', 'Plot data'  , 'CallBack', complot);
+    uimenu( submenu, 'Label', 'Output TEP peak analysis (group)'  , 'CallBack', compeakoutputgroup, 'separator', 'on');
+    uimenu( submenu, 'Label', 'Plot data (group)'  , 'CallBack', complotgroup);
+    
