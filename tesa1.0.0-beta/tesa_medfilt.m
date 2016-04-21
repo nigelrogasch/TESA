3bb0434f560ba62d6ad54a8eb482b63956a7d71e
Nigel Rogasch <nigel.rogasch@gmail.com>
2016-04-22 09:44:58 +1000

    Included instructions for medfilt

--- tesa1.0.0-beta/tesa_medfilt.m
+++ tesa1.0.0-beta/tesa_medfilt.m
@@ -1,3 +1,45 @@
+% tesa_medfilt()      - applies a median filter over a given data window.
+%                           The median filter takes in to account data either side of 
+%                           window according to the filter order and does not zero pad.
+% 
+% Usage:
+%   >>  out = tesa_medfilt( data, win, filtOrd );
+%
+% Inputs:
+%   data                 - data matrix n x m where n is channels and m is
+%                       all trial conactenated in to a single time course.
+%   win                 - win is a n x m data matrix where n is channels and m
+%                       are the time points for filtering
+%   filtOrd             - integer describing the filter order. The filter
+%                       order determines how many samples are used for the
+%                       median filter. The filter order follow the Matlab
+%                       median filter convention for odds and evens: 
+%                       When filtOrd is odd, y(k) is the median of x(k-(filtOrd-1)/2:k+(filtOrd-1)/2).
+%                       When filtOrd is even, y(k) is the median of x(k-filtOrd/2:k+(filtOrd/2)-1).
+% 
+% Outputs:
+%   out                 - data matrix n x m the same size as win with the
+%                       filtered data.
+%
+% See also:
+%   tesa_filtmedian 
+
+% Copyright (C) 2016  Nigel Rogasch, Monash University,
+% nigel.rogasch@monash.edu
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 function out = tesa_medfilt(data,win,filtOrd)
 
